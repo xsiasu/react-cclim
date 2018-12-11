5204a28ce7c324ba1f090cf7935e7e278c47aa96
xsiasu <xsiasu@gmail.com>
2018-12-11 17:33:19 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+
+  state = {}
+
+  componentDidMount() {
+    this._getMovies()
+  }
+  
+  _callApi = () => {
+    return(
+      fetch("https://res.cloudinary.com/dlrv4nsyi/raw/upload/v1544429554/data/json/products.json")
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .then(err => {
+        console.log(err)
+      })
+
+    )
+
+  }
+
+  _getMovies = async() => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
+
+
         </header>
       </div>
     );
